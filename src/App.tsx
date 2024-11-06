edbdfb95453e8c42636651447cd6c1902bea3c37
Gaku-N <naito@hosiden.com>
2024-11-06 21:50:28 +0900

    画面機能追加。モーダル。トースト

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { MenuLogin } from './components/organisims/login/MenuLogin';
 import { UserList } from './components/organisims/user/UserList';
+import { Toaster } from './components/ui/toaster';
 function App() {
   return (
     <>
+      <MenuLogin />
       <UserList />
+      <Toaster />
     </>
   );
 }
--- src/components/organisims/login/MenuLogin.tsx
+++ src/components/organisims/login/MenuLogin.tsx
@@ -1,13 +1,21 @@
-import { IconMenu } from "@/components/molecules/iconMenu";
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox";
-import { Box, Center, Flex, Heading, HStack, Input, Link, Stack, Text } from "@chakra-ui/react"
-import axios from "axios";
-import { memo, useState } from "react";
+import { IconMenu } from '@/components/molecules/IconMenu';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { useMessage } from '@/hooks/useMessage';
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Input,
+  Link,
+  Stack,
+} from '@chakra-ui/react';
+import axios from 'axios';
+import { memo } from 'react';
 
 export const MenuLogin = memo(() => {
-
-  const [count, incCount] = useState(0);
   // 重たいデータ
   const data = [...Array(2000).keys()];
   data.forEach(() => {
@@ -16,56 +24,80 @@ export const MenuLogin = memo(() => {
 
   //外部からデータを取得
   const fetchData = () => {
-    axios.get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos').then((res) => {
-      console.log(res);
-    })
-  }
+    axios
+      .get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        console.log(res);
+      });
+  };
 
   type TodoType = {
     userId: number;
     id: number;
     title: string;
     complete: boolean;
-  }
- 
+  };
+
   fetchData();
 
-
+  const { showMessage } = useMessage();
   const onClickButton = () => {
-    console.log('ボタンが押された')
-    incCount(count + 1);
-  }
+    showMessage({title: 'ログイン', description: 'ログインに成功しました', type: 'success'})
+  };
 
   return (
-    <Flex alignItems='center' justifyContent='center' height='100vh'>
-      <Box bg='white' w='sm' p={4} borderRadius='md' shadow='md'>
-        <Heading>
+    <>
+      {/* <Toaster /> */}
+      <Flex
+        alignItems='center'
+        justifyContent='center'
+        height='100vh'
+      >
+        <Box
+          bg='white'
+          w='sm'
+          p={4}
+          borderRadius='md'
+          shadow='md'
+        >
+          <Heading>
             <Center>
-          <HStack gap={4}>
-              <IconMenu imgPath='/vite.svg' />
-          </HStack>
+              <HStack gap={4}>
+                <IconMenu imgPath='/vite.svg' />
+              </HStack>
             </Center>
-          <Center fontSize='12px'>Not a member yet?
-            <Link pl={2} href="#">Sign Up here</Link>
-          </Center>
-          <Stack gap={6} py={4} px={10}>
-            <Input placeholder="ユーザーID" />
-            <Input placeholder="パスワード" />
-            <Checkbox>Save Password</Checkbox>
-            <Button 
-              bg='teal.400'
-              color='white'
-              _hover={ {opacity: 0.8}}
-              onClick={onClickButton}
-            >ログイン</Button>
             <Center fontSize='12px'>
-              <Link href="#">Forget Password?</Link>
+              Not a member yet?
+              <Link
+                pl={2}
+                href='#'
+              >
+                Sign Up here
+              </Link>
             </Center>
-            カウント値：{count}
-          </Stack>
-        </Heading>
-      </Box>
-    </Flex>
-
-  )
-})
+            <Stack
+              gap={6}
+              py={4}
+              px={10}
+            >
+              <Input placeholder='ユーザーID' />
+              <Input placeholder='パスワード' />
+              <Checkbox>Save Password</Checkbox>
+              <Button
+                bg='teal.400'
+                color='white'
+                _hover={{ opacity: 0.8 }}
+                onClick={onClickButton}
+              >
+                ログイン
+              </Button>
+              <Center fontSize='12px'>
+                <Link href='#'>Forget Password?</Link>
+              </Center>
+            </Stack>
+          </Heading>
+        </Box>
+      </Flex>
+    </>
+  );
+});
